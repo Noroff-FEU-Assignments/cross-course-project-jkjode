784c6531b511fcca47f91cbad4c85a3bfe66ab71
jkjode <104823459+jkjode@users.noreply.github.com>
2022-12-18 19:34:08 +0100

    JS updates
    created several JS files and implemented them in the html.

---
+++ js/jacketSpecific.js
@@ -0,0 +1,49 @@
+import { jackets } from "./allJackets.js";
+import { cart } from "./cart.js";
+
+{
+  /* <div class="productbox">
+  <a href="jacket_1.html" >
+  <img src="/images/products/RainyDays_Jacket1.png" alt="The Strong Jacket" class="product1"></img>
+</a>
+  <div class="nametag"><h2>The Strong Jacket<p class="pricetag">$175</p></h2></div>
+</div> */
+}
+
+const addToCartButton = document.getElementById("add-to-cart");
+const productBox = document.getElementById("productbox");
+const jacketSpecific = document.getElementById("product-box");
+
+function displayJackets() {
+  Object.keys(jackets).forEach((jacketId) => {
+    const jacket = jackets[jacketId];
+    productSection.innerHTML += `
+        <div class=jacket-display">
+        <a href="./allJackets.html?id=${jacketId}">
+        <p class="display-title">${jacket.name}</p>
+        <img src="${jacket.image}" alt="Image of the ${jacket.name} jacket" />
+        <p class="jacket-price">Price <strong>$${jacket.jacketprice}</strong></p>
+        </a>
+        </div>`;
+  });
+}
+
+if (displayJackets) {
+  displayJackets();
+  displayJackets();
+}
+
+if (jacketSpecific) {
+  const jacketId = new URLSearchParams(window.location.search).get("id");
+  const jacket = jacket[jacketId];
+  jacketSpecific.innerHTML = `
+    <div>
+    </div>
+    `;
+  jacketHeader.innerHTML = `
+    ${jacket.name.toUpperCase()}`;
+  addToCartButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    addToCartButton(jacketId);
+  });
+}
